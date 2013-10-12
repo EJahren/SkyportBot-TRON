56bcef255579cc38aa3b2bc18d13a35f8f785585
Paul Grant <poglesbyg@localhost.localdomain>
2013-10-12 14:51:25 +0200

    Greedy-Chooser first commit

---
+++ src/main/java/game/Greedy_Chooser.java
@@ -0,0 +1,18 @@
+package game;
+
+
+import skyport.api.game.GameState;
+import skyport.api.game.Map;
+import skyport.api.game.Player;
+
+public class Greedy_Chooser {
+	
+	public static Decision choose (GameState state) {
+		
+		Player me = state.getPlayers().get(0);
+		Map map = new Map();
+		
+		
+		return null;
+	}
+}
